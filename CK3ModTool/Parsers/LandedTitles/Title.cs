030a612705e7e4552b752ecf1a54d4cd8862f89a
Nathan Newcomer <newcomer.62@buckeyemail.osu.edu>
2021-08-26 16:49:55 -0400

    Add project files.

---
+++ CK3ModTool/Parsers/LandedTitles/Title.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace CK3ModTool.Parsers.LandedTitles
+{
+    class Title
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Color Color2 { get; set; }
+        public Dictionary<string, string> Attributes { get; set; }
+    }
+}
